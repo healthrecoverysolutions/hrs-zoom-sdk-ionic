767f443f6489cd4f20a33d7810acbfefa7e07b3a
Anubhav <asaxena@healthrecoverysolutions.com>
2024-05-23 15:05:57 +0530

    Changed label name and removed unwanted code comments.

--- src/ios/CustomMessageComponent.m
+++ src/ios/CustomMessageComponent.m
@@ -1,36 +1,30 @@
-//
-//  WaitingMessageView.m
-//  PatientConnect Mobile
-//
-//  Created by Anubhav Saxena on 21/05/24.
-//
 
 #import "CustomMessageComponent.h"
 
 @implementation CustomMessageComponent
 
-UILabel *waitingMessageLabel;
+UILabel *messageLabel;
 NSString *messageLabelText = @"";
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
     // Drawing code
-    waitingMessageLabel = [[UILabel alloc] initWithFrame:CGRectMake(250, 100, 200, 40)];
-    waitingMessageLabel.textColor = UIColor.whiteColor;
-    waitingMessageLabel.font = [UIFont boldSystemFontOfSize:16];
-    waitingMessageLabel.backgroundColor = UIColor.blackColor;
-    [waitingMessageLabel setTextAlignment:NSTextAlignmentCenter];
-    [waitingMessageLabel.layer setCornerRadius:10.0];
-    [waitingMessageLabel setClipsToBounds:YES];
-    [waitingMessageLabel setAlpha:0.7];
-    [waitingMessageLabel setHidden:NO];
-    waitingMessageLabel.text = messageLabelText;
-    [self addSubview:waitingMessageLabel];
-    [waitingMessageLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [[waitingMessageLabel.widthAnchor constraintEqualToConstant:250] setActive:YES];
-    [[waitingMessageLabel.heightAnchor constraintEqualToConstant:50] setActive:YES];
-    [[waitingMessageLabel.centerXAnchor constraintEqualToAnchor:self.centerXAnchor] setActive:YES];
-    [[waitingMessageLabel.topAnchor constraintEqualToAnchor:self.topAnchor constant:125] setActive:YES];
+    messageLabel = [[UILabel alloc] initWithFrame:CGRectMake(250, 100, 200, 40)];
+    messageLabel.textColor = UIColor.whiteColor;
+    messageLabel.font = [UIFont boldSystemFontOfSize:16];
+    messageLabel.backgroundColor = UIColor.blackColor;
+    [messageLabel setTextAlignment:NSTextAlignmentCenter];
+    [messageLabel.layer setCornerRadius:10.0];
+    [messageLabel setClipsToBounds:YES];
+    [messageLabel setAlpha:0.7];
+    [messageLabel setHidden:NO];
+    messageLabel.text = messageLabelText;
+    [self addSubview:messageLabel];
+    [messageLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [[messageLabel.widthAnchor constraintEqualToConstant:250] setActive:YES];
+    [[messageLabel.heightAnchor constraintEqualToConstant:50] setActive:YES];
+    [[messageLabel.centerXAnchor constraintEqualToAnchor:self.centerXAnchor] setActive:YES];
+    [[messageLabel.topAnchor constraintEqualToAnchor:self.topAnchor constant:125] setActive:YES];
     [self setUserInteractionEnabled:false];
 }
 
